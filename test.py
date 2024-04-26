0a82b87211810d332b4b6d78f19c7ea3cb02d5e9
Yagizko <152534379+Yagizko@users.noreply.github.com>
2024-04-26 21:58:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("HELLO WORLD")
+liste = {"CRİNGE":"Garip Veya Utandırıcı Birşey",
+        "LOL":"Komik Bir Şeye Verilen Cevab"}
+
+kelime = input("Hangi Kelimenin anlamını öğrenmek istiyorsun?")
+
+if kelime in liste.keys():
+  print("Sorduğunuz Kelimenin Anlamı =",liste[kelime])
+else:
+  print("Böyle bir kelime yok!")
